fb018c87ab6b2cc24f13599fe8c70b997e1cdb5e
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-11-15 03:27:03 -0500

    Update SelectionSort.py

--- Sorting/SelectionSort.py
+++ Sorting/SelectionSort.py
@@ -2,7 +2,7 @@
 
 array = [13, 46, 24, 52, 20, 9]
 
-def SelectionSort(array) -> list:
+def selectionSort(array) -> list:
     
     n = len(array)
     for i in range(0, n-1):
@@ -19,7 +19,7 @@ def SelectionSort(array) -> list:
 
 print("Given Unsorted Array: ", array)
 print()
-print("After applying Selection Sort: ", SelectionSort(array))
+print("After applying Selection Sort: ", selectionSort(array))
 
 # Selection sort is a simple and efficient sorting algorithm 
 # it works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list. 
