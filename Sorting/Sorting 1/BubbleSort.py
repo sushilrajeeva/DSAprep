b18756c8ac9cab2a3cb8fe6c79b144899377a8e7
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-11-25 07:39:02 -0500

    Bubble Sort using Recursion

--- Sorting/Sorting 1/BubbleSort.py
+++ Sorting/Sorting 1/BubbleSort.py
@@ -35,3 +35,23 @@ print("After applying Bubble Sort: ", bubbleSort(array))
 
 # Time: O(n*n)
 # Space: O(1)
+
+# Implementing the same using Recursion
+
+def bubbleSortRecursion(array, n):
+    # Base condition
+    if n == 1:
+        return
+    
+    # One pass of bubble sort. After this pass, the largest element is moved (or bubbled) to end.
+    for i in range(0, n-1):
+        if array[i] > array[i+1]:
+            array[i], array[i+1] = array[i+1], array[i]
+
+    # Largest element is fixed, recursion for remaining array
+    bubbleSortRecursion(array, n - 1)
+
+array = [13, 46, 24, 52, 20, 9]
+print("Given Unsorted Array:", array)
+bubbleSortRecursion(array, len(array))
+print("After applying Bubble Sort using Recursion:", array)
