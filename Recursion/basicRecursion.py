d0028f47dc0c6956486630ed6a7f320c4920c049
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-11-26 04:26:32 -0500

    Update basicRecursion.py

--- Recursion/basicRecursion.py
+++ Recursion/basicRecursion.py
@@ -96,3 +96,112 @@ def rightTriangle(rows: int, current_row: int = 1):
 height = 5
 print("Right Triangle with height", height, ":")
 rightTriangle(height)
+
+
+# Reversing a linked list
+
+class Node:
+    
+    def __init__(self, value, next = None):
+        self.value = value
+        self.next = next
+        
+# printing linkedlist using recursion
+
+def print_nodes(head: Node):
+    
+    # Exit condition
+    if(head.next is None):
+        print(head.value)
+        return 
+    
+    # Minimum work
+    print(head.value, "-> ", end="")
+    return print_nodes(head.next)
+        
+
+def reverseLinkedList(current: Node, previous: Node = None) -> Node:
+    
+    #Exit Condition
+    if(current is None):
+        return previous
+    
+    # saving the next node
+    next = current.next
+    
+    current.next = previous
+    
+    return reverseLinkedList(next, current)
+
+
+a = Node('A')
+b = Node('B')
+c = Node('C')
+d = Node('D')
+e = Node('E')
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+    
+print()
+print("************ Reversing a Linked List ************")
+print("Given Linked list: ")
+print_nodes(a)
+reversed_head = reverseLinkedList(a)
+print("Reversed Linked list: ")
+print_nodes(reversed_head)
+
+# Merging two sorted Linked Lists
+
+def mergeSortedList(A: Node, B: Node) -> Node:
+    
+    # Exit Condition
+    if(A is None): return B
+    if(B is None): return A
+    
+    # Minimum work
+    if(A.value <= B.value):
+        A.next = mergeSortedList(A.next, B)
+        return A
+    else:
+        B.next = mergeSortedList(A, B.next)
+        return B
+        
+    
+    
+
+        
+a = Node(1)
+b = Node(1)
+c = Node(2)
+d = Node(3)
+e = Node(6)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e      
+
+
+x = Node(2)
+y = Node(2)
+z = Node(4)
+
+x.next = y
+y.next = z
+
+
+
+print()
+print("************ Merging two sorted Linked Lists ************")
+print("Linked list A: ")
+print_nodes(a)
+print()
+print("************ Merging two sorted Linked Lists ************")
+print("Linked list B: ")
+print_nodes(x)
+print()
+print("Sorted Linked List: ")
+print_nodes(mergeSortedList(a,x))
