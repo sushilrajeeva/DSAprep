1066c2414dfe45c93cecf7de7a5351827aed6927
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-12-31 00:24:36 -0500

    Array Rotation

--- PythonBasics/dictionaryPractice.py
+++ PythonBasics/dictionaryPractice.py
@@ -10,3 +10,18 @@ def gen_password() ->  str:
 data = [{key: index if key == "id" else value if key == "username" else gen_password() for key in keys} for index, value in enumerate(users)]
 
 print(data)
+
+
+from sys import *
+from collections import *
+from math import *
+
+def rotateArray(arr: list, n: int) -> []:
+    # Write your code from here.
+    first_ele = arr[0]
+    arr = [arr[index] for index in range(1,n)]
+    arr.append(first_ele)
+    print(arr)
+    return arr
+
+print(rotateArray([1, 2, 3, 4, 5], 5))
