4b4bbafdf404a5830fed208f24d1bf06d492f033
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-12-31 03:30:10 -0500

    Moving all non zero to end
    Problem link -> https://takeuforward.org/data-structure/move-all-zeros-to-the-end-of-the-array/

--- PythonBasics/arrayPractice.py
+++ PythonBasics/arrayPractice.py
@@ -37,3 +37,28 @@ def max_ones(arr: list):
 
 print(max_ones(input))
 print(max_ones(input1))
+
+def moveZeros(n: int,  a: list) -> list:
+    
+    j = -1
+    for i in range(n):
+        if a[i] == 0:
+            j= i 
+            break
+        
+    
+    if j == -1:
+        return a
+    
+    for i in range(j+1, n):
+        if a[i] != 0:
+            a[i], a[j] = a[j], a[i]
+            j+=1
+            
+    return a
+
+
+
+input = [0,0,0,1]
+
+print(moveZeros(4, input))
