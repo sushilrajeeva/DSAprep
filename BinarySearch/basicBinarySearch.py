32ab5d1ede14767e68882d349abe75009fac1a56
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-01-08 05:41:26 -0500

    Lower Bound using Binary Search

--- BinarySearch/basicBinarySearch.py
+++ BinarySearch/basicBinarySearch.py
@@ -79,6 +79,64 @@ target = 53
 
 # print(binary_search(arr, target))
 print(binary_search_using_recursion(arr, target))
+
+# Lower Bound
+# Problem statement
+# You are given an array 'arr' sorted in non-decreasing order and a number 'x'.
+
+
+
+# You must return the index of lower bound of 'x'.
+
+
+
+# Note:
+# For a sorted array 'arr', 'lower_bound' of a number 'x' is defined as the smallest index 'idx' such that the value 'arr[idx]' is not less than 'x'
+
+# If all numbers are smaller than 'x', then 'n' should be the 'lower_bound' of 'x', where 'n' is the size of array.
+
+# Consider 0-based indexing.
+
+
+# Example:
+# Input: ‘arr’ = [1, 2, 2, 3] and 'x' = 0
+
+# Output: 0
+
+# Explanation: Index '0' is the smallest index such that 'arr[0]' is not less than 'x'.
+
+def lower_bound(arr: [int], x: int) -> int:
     
+    n = len(arr)
+    
+    # Initializing my pointers
+    low, high = 0, n - 1
+    
+    # Initializing answer to size of the array
+    answer = n
+    
+    while low <= high:
+        
+        mid = int(low + (high - low) // 2)
+        
+        if x <= arr[mid]:
+            high = mid - 1
+            answer = mid
+            
+            
+        else:
+            low = mid + 1
+    
+            
+    return answer
+
+n = 76
+arr = [366, 482, 727, 813, 1133, 1219, 1257, 1640, 2084, 2110, 2363, 2559, 2706, 2763, 3471, 4213, 5300, 5365, 5433, 5517, 6227, 6506, 8384, 8438, 8687, 8847, 8873, 9963, 10696, 11582, 12221, 12344, 12791, 14072, 14079, 14239, 14387, 14949, 15244, 16945, 17516, 19637, 20011, 20504, 20687, 20959, 21252, 21570, 21620, 21651, 22922, 23369, 24141, 24852, 25012, 25192, 25496, 25694, 25843, 27359, 27368, 27523, 28123, 28339, 28568, 29072, 29560, 30259, 30321, 30388, 30918, 31410, 31748, 32370, 32512, 32647]
+x = 17111
+
+print(lower_bound(arr, x))
+
+
+
     
     
