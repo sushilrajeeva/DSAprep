e1918440e60388f75083313e42c69d1512b6f16c
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-01-30 03:11:13 -0500

    Deleting head/start of the doubly linked list

--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -43,6 +43,27 @@ head = arr_to_doubly_linked_list(arr)
 
 print("Initial Array:", arr)
 print("After converting array to Doubly Linked List:", print_doubly_linked_list(head))
+
+
+# 3. Delete element at the start/head of doubly linked list
+
+def del_head(head: DoublyLinkedListNode) -> DoublyLinkedListNode:
+    
+    if not head or not head.next: 
+        return None
+    
+    previous = head
+    head = head.next # The second node becomes the new head
+    head.previous = None # Disconnect the new head's previous link
+    previous.next = None # Clean up the old head's next link
+    
+    
+    return head
+
+arr = [1, 2, 3, 4, 5, 6]
+head = arr_to_doubly_linked_list(arr)
+print("Doubly Linked List before deleting head:", print_doubly_linked_list(head))
+print("Doubly Linked List after deleting head:", print_doubly_linked_list(del_head(head)))
         
     
     
