bd591b4fa42862d6cb22e345ff094cc72e210404
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-02-07 02:30:44 -0500

    Fibbonacci

--- Recursion/RecursionBasics.py
+++ Recursion/RecursionBasics.py
@@ -57,5 +57,42 @@ def fact(n: int) -> int:
 
 print("Factorial of a given number")
 print(fact(5))
+
+
+
+# Calculate the nth fibonacci sequence term
+
+def fib(n: int) -> int:
     
+    if n == 0 or n == 1:
+        return n
+    
+    # Faith
+    partialFirst = fib(n-1)
+    partialSecond = fib(n-2)
+    return partialFirst + partialSecond
+    
+print("Fibbonacci number of the nth term")
+print(fib(5))
+
+# Print fibbonacci sequence of n numbers 
+def fib_seq(n: int) -> None:
+    
+    a, b = 0, 1
+    print(a)
+    print(b)
+    return fibo(n - 2, a, b)
+
+def fibo(n: int, a: int = 0, b: int = 1) -> None:
+    
+    if n == 0:
+        return
+    
+    
+    c = a + b
+    print(c)
+    fibo(n-1, b, c)
+    
+print("Printing fibbonacci sequence of n numbers")
+fib_seq(5)
     
