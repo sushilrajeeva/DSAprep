d7ff4d0ad2e3c3560e1b70e74a7f8ce0583e2718
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-02-09 06:25:12 -0500

    Get all the paths to the maze

--- Recursion/RecursionAdvance.py
+++ Recursion/RecursionAdvance.py
@@ -1,35 +1,4 @@
 from typing import *
-# Count total paths in maze to move from (0, 0) to (n, m)
-
-def count_maze(n: int, m: int) -> None:
-    
-    def recursive(i: int = 0, j: int = 0) -> int:
-        
-        # Base Condition
-        if i == n or j == m:
-            return 0
-        
-        if i == n - 1 and j == m - 1:
-            return 1
-        
-        
-        # Faith
-        # Move down
-        downPaths = recursive(i+1, j)
-        
-        # Move right
-        rightPaths = recursive(i, j+1)
-        
-        return downPaths + rightPaths
-    
-    result = recursive()
-    print(result)
-
-n, m = 3, 3
-print(f"Count total paths in maze to move from (0, 0) to ({n}, {m})")
-
-count_maze(n, m)
-
 
 # Get all the stair paths given a number - valid steps are 1 , 2 and 3 only
 
@@ -68,6 +37,75 @@ def stair_path(n: int) -> List[str]:
 n = 4   
 print(f"All the possible valid paths for given number {n}")
 print(stair_path(n))
+
+
+# Count total paths in maze to move from (0, 0) to (n, m)
+
+def count_maze(n: int, m: int) -> None:
+    
+    def recursive(i: int = 0, j: int = 0) -> int:
+        
+        # Base Condition
+        if i == n or j == m:
+            return 0
+        
+        if i == n - 1 and j == m - 1:
+            return 1
+        
+        
+        # Faith
+        # Move down
+        downPaths = recursive(i+1, j)
+        
+        # Move right
+        rightPaths = recursive(i, j+1)
+        
+        return downPaths + rightPaths
+    
+    result = recursive()
+    print(result)
+
+n, m = 3, 3
+print(f"Count total paths in maze to move from (0, 0) to ({n}, {m})")
+
+count_maze(n, m)
+
+
+# Get all the paths to the maze
+
+def get_maze_paths(n: int, m: int, i: int = 0, j: int = 0) -> List[str]:
+    
+    # Base Condition
+    if i == n or j == m:
+        return []
+    
+    if i == n - 1 and j == m - 1:
+        return [""]
+    
+    
+    
+    # Faith
+    rightPaths = get_maze_paths(n, m, i, j + 1)
+    downPaths = get_maze_paths(n, m, i + 1, j)
+    
+    paths = []
+    
+    for path in rightPaths:
+        paths.append("R" + path)
+        
+    for path in downPaths:
+        paths.append("D" + path)
+        
+    return paths
+
+n, m = 3, 3
+print("Get all the paths to the maze of dimension {n} x {m}")
+print(get_maze_paths(n, m))
+
+
+
+
+
     
         
         
