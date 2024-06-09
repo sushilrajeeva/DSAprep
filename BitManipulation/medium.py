2a05b2692642764860c1641b8133f323d5253225
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2024-06-09 03:20:10 -0400

    Minimum bits flip to match goal

---
+++ BitManipulation/medium.py
@@ -0,0 +1,37 @@
+from typing import *
+
+counter = 0
+def new_block(message: str) -> None:
+    global counter
+    counter += 1
+    print()
+    print(f"********************** {counter}. {message} **********************")
+
+# Minimum Bits to convert to a number
+new_block("Minimum Bits to convert to a number")
+
+def minimum_bit_flip(start: int, goal: int) -> int:
+    
+    # Step 1: XOR the start and goal to set all the bits that needs to be changed, bits that don't have to be changed will be 0
+
+    compute = start ^ goal
+
+    # Step 2: Write logic to count all the set bits:
+
+    def count_set_bits(num: int) -> int:
+        
+        count = 0
+        while num:
+            # add the 0th bit in every itteration
+            count += num & 1
+            num = num >> 1 # right shifting to remove the 0th bit
+        return count
+    
+    res = count_set_bits(compute)
+    return res
+
+start = 7
+goal = 4
+print(f"Given start: {start} and goal: {goal}")
+print("Number of bits required to flip:", minimum_bit_flip(start, goal))
+
